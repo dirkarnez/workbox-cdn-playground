eaae0c111816a5e22b3ae43da1d451990021ff0f
dirkarnez <smalldirkalex@gmail.com>
2024-02-15 22:28:19 +0800

    - upload files

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.2.0/workbox-sw.js');
-
-// Precache static assets
-workbox.precaching.precacheAndRoute([
-  { url: '/path/to/file1', revision: '12345' },
-  { url: '/path/to/file2', revision: '67890' },
-  // Add more files to precache as needed
-]);
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
 
 // Cache runtime requests
 workbox.routing.registerRoute(
-  new RegExp('/api/'),
-  new workbox.strategies.NetworkFirst()
+  new RegExp('/files/'),
+  new workbox.strategies.CacheFirst()
 );
